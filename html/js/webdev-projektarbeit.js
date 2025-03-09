9a0c6ad81b946d7e7c8eeb9ccd087eb7aad87d79
Ines Jaritz <ines@aberoho.at>
2025-03-09 15:48:59 +0100

    Index.php anlegen, Dateien und Ordner aufräumen

--- html/js/webdev-projektarbeit.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navigation = document.querySelector('.navigation');
-
-    menuToggle.addEventListener('click', () => {
-        const isOpen = navigation.classList.toggle('open');
-        menuToggle.setAttribute('aria-expanded', isOpen); // Accessibility: Aktualisierung
-    });
-});
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<?php get_header(); ?>
+
+    <div class="main-container">
+        <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <article>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p><?php _e('Keine Beiträge gefunden.', 'interior-design-translation'); ?></p>
+        <?php endif; ?>
+    </div>
+
+<?php get_footer(); ?>
 <?php
